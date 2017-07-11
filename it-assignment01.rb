7c71d11b30efb4af051d15301ed6d09866551a6f
ahaha0807 <b5331@oic.jp>
2017-07-11 15:36:37 +0900

    add multi input process
    [step-0]
    - 投入を複数回出来る処理の追加
    - 投入金額の総計を取得できる

--- it-assignment01.rb
+++ it-assignment01.rb
@@ -2,19 +2,39 @@ def to_harf_number(input)
     input.tr('０-９', '0-9').to_i
 end
 
-total = 0
-
-puts 'お金を投入してください'
-input_money = gets.chomp!
-input_money = to_harf_number(input_money)
-
-until input_money > 0 do
-    puts '1以上の整数値を入力してください'
+def accept_money
+    puts 'お金を投入してください'
     input_money = gets.chomp!
     input_money = to_harf_number(input_money)
+    while input_money.zero? do
+        puts '1以上の整数値を入力してください'
+        input_money = gets.chomp!
+        input_money = to_harf_number(input_money)
+    end
+
+    puts input_money.to_s + '円投入されました'
+    return input_money
 end
 
-puts input_money.to_s + '円投入されました'
-total += input_money
+def ask_finish
+    puts 'お金をまだ入れますか？ (はい/いいえ)'
+    answer = gets.chomp!.to_s
+    if answer == 'はい' || answer.downcase == 'yes'
+        return true
+    elsif answer == 'いいえ' || answer.downcase == 'no'
+        return false
+    else
+        ask_finish()
+    end
+end
+
+total = 0
+isRepeat = true
+
+while isRepeat do
+    input = accept_money()
+    isRepeat = ask_finish()
+    total += input
+end
 
 puts '合計' + total.to_s + '円投入されています。'
