92ecc2cd979b9827cf6268eafe406bd90dbd3d28
ahaha0807 <b5331@oic.jp>
2017-07-15 12:05:18 +0900

    Refactoring
    [step-0]
    - リファクタリング

--- it-assignment01.rb
+++ it-assignment01.rb
@@ -1,32 +1,47 @@
-Possible_money_type = [10, 50, 100, 500, 1000]
+class Checker 
+    @@Possible_money_type = [10, 50, 100, 500, 1000]
 
-def to_harf_number(input)
-    input.tr('０-９', '0-9').to_i
+    def self.get_possible_money_type
+        return @@Possible_money_type
+    end
+
+    def self.to_harf_number(input)
+        input.tr('０-９', '0-9').to_i
+    end
+
+    def self.check_possible_money(input)
+        return @@Possible_money_type.any? {|item| item == input}
+    end
+    
+    def self.check(input)
+        input = to_harf_number(input)
+        input = 0 unless check_possible_money(input) 
+
+        while input.zero? do
+            puts '以下の' + @@Possible_money_type.size.to_s + '種類の中から選択してください'
+            @@Possible_money_type.each {|item| puts "- " + item.to_s + "円\n"}
+           
+            input = gets.chomp!
+            
+            input = to_harf_number(input)
+            input = 0 unless check_possible_money(input) 
+        end
+        return input
+    end
 end
 
-def check_possible_money(input)
-    return Possible_money_type.any? {|item| item == input}
-end
 
 def accept_money
-    output_guide = "【お金を投入してください。】\n"
-    output_guide <<  "投入できる種類は以下の" + Possible_money_type.size.to_s + "種類のみです\n"
-    Possible_money_type.each {|item| output_guide << "- " + item.to_s + "円\n" }
-    output_guide.freeze
+    possible_money_type = Checker.get_possible_money_type()
 
+    output_guide = "【お金を投入してください。】\n"
+    output_guide <<  "投入できる種類は以下の" + possible_money_type.size.to_s + "種類のみです\n"
+    possible_money_type.each {|item| output_guide += "- " + item.to_s + "円\n"}
     puts output_guide
     
     input_money = gets.chomp!
-    input_money = to_harf_number(input_money)
-    input_money = 0 unless check_possible_money(input_money) 
 
-    while input_money.zero? do
-        puts '以下の' + Possible_money_type.size.to_s + '種類の中から選択してください'
-        Possible_money_type.each {|item| puts "- " + item.to_s + "円\n"}
-        input_money = gets.chomp!
-        input_money = to_harf_number(input_money)
-        input_money = 0 unless check_possible_money(input_money) 
-    end
+    input_money = Checker.check(input_money)
 
     puts input_money.to_s + '円投入されました'
     return input_money
@@ -35,9 +50,9 @@ end
 def ask_finish
     puts 'お金をまだ入れますか？ (はい/いいえ)'
     answer = gets.chomp!.to_s
-    if answer == 'はい' || answer.downcase == 'yes'
+    if answer == 'はい' || answer.downcase == 'yes' || answer.downcase == 'y'
         return true
-    elsif answer == 'いいえ' || answer.downcase == 'no'
+    elsif answer == 'いいえ' || answer.downcase == 'no' || answer.downcase == 'n'
         return false
     else
         ask_finish()
