4af7819b0967ba35f3da7faf6507aace270a02a1
ahaha0807 <b5331@oic.jp>
2017-07-17 17:47:06 +0900

    Fix admin mode words
    - 管理者モードの文言を修正

--- stocker.rb
+++ stocker.rb
@@ -6,6 +6,7 @@ class Stocker
     end
 
     def self.display_lanes
+        puts ''
         @@Lanes.each.with_index {|item, index|
             puts 'Lane-' + index.to_s
             puts item.get_data()
